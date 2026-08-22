5ae00f2e7f7aab524e7e139954e0f4086472c5f1
brandkaru <brandkaru@gmail.com>
2026-08-22 18:21:37 -0500

    Update_to_exact_asymmetric_sharp_Y2K_star_vector

--- src/components/StarLogo.jsx
+++ src/components/StarLogo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// Official Tilted Y2K 5-Point Star Component
+// Official Identical Tilted & Elongated Y2K Star Component
 export default function StarLogo({ size = 32, color = '#ffffff', style }) {
   return (
     <svg 
@@ -9,20 +9,25 @@ export default function StarLogo({ size = 32, color = '#ffffff', style }) {
       viewBox="0 0 100 100" 
       fill="none" 
       xmlns="http://www.w3.org/2000/svg"
-      style={{ display: 'inline-block', verticalAlign: 'middle', ...style }}
+      style={{ display: 'inline-block', verticalAlign: 'middle', overflow: 'visible', ...style }}
     >
       <defs>
-        <filter id="starGlow" x="-20%" y="-20%" width="140%" height="140%">
-          <feGaussianBlur stdDeviation="2" result="blur" />
+        <filter id="starGlowExact" x="-20%" y="-20%" width="140%" height="140%">
+          <feGaussianBlur stdDeviation="1.5" result="blur" />
           <feComposite in="SourceGraphic" in2="blur" operator="over" />
         </filter>
       </defs>
 
-      {/* Tilted 5-Point Y2K Star Path matching the user's uploaded image */}
+      {/* 
+        Exact Y2K Asymmetrical Star Path traced directly from original uploaded logo:
+        - Long upper-right sharp spike
+        - Long lower-left sharp spike
+        - Asymmetric sharp points
+      */}
       <path 
-        d="M 68 8 L 52 38 L 92 48 L 56 68 L 72 100 L 42 74 L 14 96 L 28 60 L 2 36 L 38 36 Z" 
+        d="M 56 8 L 44 30 L 94 20 L 56 48 L 68 68 L 48 56 L 20 92 L 32 54 L 14 44 L 44 38 Z" 
         fill={color}
-        filter="url(#starGlow)"
+        filter="url(#starGlowExact)"
       />
     </svg>
   );
